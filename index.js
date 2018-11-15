3d929db2a230f0cc6c28b22f138eb78462b0eb62
Fei Qi <feiqi9047@gmail.com>
2018-11-15 22:34:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,16 @@ function logShout(string){
 function logWhisper(string){
   return console.log("hello")
 }
+
+function sayHiToGrandma(string){
+  if(string="I love you, Grandma."){
+    return "I love you, too."
+  }else if
+    (string.ToLowerCase===mixedCase){
+    return "I can\'t hear you!"
+  }else{
+    (string.toUpperCase===mixedCase){
+      return "YES INDEED!"
+    }
+  }
+}

5a4d9393f3204cddc7e8b193bb333ed996c825ce
Fei Qi <feiqi9047@gmail.com>
2018-11-15 22:36:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function sayHiToGrandma(string){
   if(string==="I love you, Grandma."){
     return "I love you, too."
   }else if
-    (string===string.ToLowerCase()){
+    (string===string.toLowerCase()){
     return "I can\'t hear you!"
   }else if
     (string===string.toUpperCase()){
